fb5618d61d845a1754e736e222fa20806b24615f
jainmani26 <93339993+jainmani26@users.noreply.github.com>
2022-01-31 01:35:00 +0530

    Update Implement Stack using Queues.java

--- Implement Stack using Queues.java
+++ Implement Stack using Queues.java
@@ -6,18 +6,18 @@ class MyStack {
     }
     
     public void push(int x) {
-        q.add(x);//add element into queue
+        q.add(x);//Push the element in the queue.
         for (int i = 0; i < q.size() - 1; i++) { //run a loop till size - 1
-            q.add(q.remove()); //remove top element and add again in a queue
+            q.add(q.remove()); //remove element from queue and again push back to the queue, hence the most recent element becomes the most former element and vice versa.
         }
     }
     
     public int pop() {
-         return q.remove();
+         return q.remove(); // remove the element from the queue.
     }
     
     public int top() {
-        return q.peek();
+        return q.peek(); //show the element at the top of the queue.
     }
     
     public boolean empty() {
